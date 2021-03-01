d8b0d3fcf8d43fafb76ac98d6469d303a6ac62c9
aharri64 <haveacompleteday@gmail.com>
2021-02-28 20:41:14 -0700

    added history aspect in game

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ class Game extends React.Component {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.serState({
+        this.setState({
             history:history.concat([{
                 squares:squares,
             }]),
@@ -76,6 +76,18 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}> { desc } </button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) {
             status = winner + ' is the winner';
@@ -93,7 +105,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{ status }</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{ moves }</ol>
                 </div>
             </div>
         );
